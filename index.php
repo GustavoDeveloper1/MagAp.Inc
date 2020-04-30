41dadd53a234ee6a7e6740f2d9abd96f23066cfd
DevInvestidor <62334910+DevInvestidor@users.noreply.github.com>
2020-04-29 21:01:00 -0300

    alteracao 29/04

--- index.php
+++ index.php
@@ -1,42 +1,72 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+      <!DOCTYPE html>
+      <html lang="pt-br">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MagAp.Inc</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+      
+        </style>
+      </head>
+      <body>
+        <nav class="navbar_horizontal">
+            <ul>
+              <li><a href="#">Contact-Us</a></li>
+              <li><a href="cadastro.html">Register</a></li>
+              <li><a href="index.php">Home</a></li> 
+            </ul>
+        </nav>
+        <div class="container-login100" >
 
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
-
-  <title>MagAp.Inc</title>
-</head>
-
-<body>
-
-  <h1>MagAp.Inc</h1>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  <div class="wrap-login100 p-t-30 p-b-50">
+    <span class="login100-form-title p-b-41">
+      User Login
+    </span>
+    <form class="login100-form validate-form p-b-33 p-t-5" action="login.php" method="POST">
+      <div class="wrap-input100 validate-input" data-validate = "Enter username">
+            <input class="input100 	" type="text" name="username" placeholder="User name">
+            <span class="focus-input100" data-placeholder="&#xe82a;"></span>
+      </div>
+      <div class="wrap-input100 validate-input" data-validate="Enter password">
+          <input class="input100" type="password" name="pass" placeholder="Password">
+          <span class="focus-input100" data-placeholder="&#xe80f;"></span>
+      </div>
+      <div class="cadastre">
+      <a href="cadastrocliente.html">Cadastre-se</a>
+      </div>
+      <div class="container-login100-form-btn m-t-32">
+      <button class="login100-form-btn">
+        Login
+      </button>
+      </div>
+      <div class="container-login100-form-btn m-t-32">
+      <button class="login100-form-btn">
+        Limpar
+      </button>
+      </div>
 
 
-  
+      </form>
+      </div>
+      </div>
+      </div>
 
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
 
-</html>
+        <footer >
+          <div class="esqueda">
+              <p class="titulo">Contatos</p>
+              <p>Phone: +55 (61) 3300-1213</p>
+          </div>
+          <div class="center">
+              <p class="titulo">Parceiros</p> <br>
+              <p class="text">Projeção</p>
+          </div>
+          <div class="right">
+              
+          </div>
+        </footer>
+      </body>
+      </html>
