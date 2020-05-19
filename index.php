f0b78cb961302a556d9983d5100b9801df615593
Pedro46 <pedrolima4680@gmail.com>
2020-05-18 23:15:58 -0300

    Telas em andamento

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -8,8 +9,8 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <title>Central do Assinante</title>
   <link rel="stylesheet" href="css/style.css">
+  <title>Central do Assinante</title>
   <link rel="shortcut icon" href="images/navbar.png" type="image/x-icon">
   <style>
     body {
