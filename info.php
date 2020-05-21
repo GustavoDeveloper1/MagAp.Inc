98769e5f3322bc57cd217dccccafa3a41bbcd49d
Pedro46 <pedrolima4680@gmail.com>
2020-05-20 21:03:32 -0300

    Novo Layout

--- info.php
+++ info.php
@@ -2,12 +2,11 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/style.css">
   <link rel="shortcut icon" href="./images/navbar.png" type="image/x-icon">
-  <title>MagAp.Inc</title>
   <style>
     #overflowTest {
       background: white;
@@ -19,11 +18,11 @@
       border: 1px solid #fafad2;
     }
   </style>
+  <title>MagAp.Inc</title>
 </head>
-
 <body>
   <!-- NavBar-->
-  <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+  <nav id="nav" class="navbar navbar-expand-lg navbar-light fixed-top">
     <a class="navbar-brand" href="#">MagAp</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
       <span class="navbar-toggler-icon"></span>
@@ -45,18 +44,29 @@
         </div>
       </div>
   </nav>
+  <br><br><br>
+  <div class="container-fluid">
+    <nav>
+      <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Funcionários</a>
+        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Fornecedores</a>
+        <a class="nav-item nav-link" id="nav-estoque-tab" data-toggle="tab" href="#nav-estoque" role="tab" aria-controls="nav-estoque" aria-selected="false">Estoque</a>
+      </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+        <p>Em Produção
+        </p>
 
-  <!-- Tab funcionários-->
-  <div class="tabs">
-    <div class="tab">
-      <input type="radio" id="tab-1" name="tab-group-1" checked>
-      <label for="tab-1">Funcionários</label>
-      <div class="content">
+      </div>
+      <!--Tabela funcionários-->
+      <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+        <br>
         <h4>Funcionários</h4>
         <a style="text-decoration: none" href="#">
           <h6>Cadastrar Funcionário</h6>
         </a>
-        <hr>
         <div id="overflowTest">
           <table id="tablefunc" class="table">
             <thead>
@@ -88,63 +98,17 @@
                 <td>(61) 9 3645-8759</td>
                 <td>Brasília-DF</td>
                 <td><button type="button" class="btn btn-warning">Editar</button> <button type="button" class="btn btn-danger">Excluir</button></td>
-              </tr>
-              <td>Marcos</td>
-              <td>Silva</td>
-              <td>17890123456</td>
-              <td>Atendente</td>
-              <td>(61) 9 3645-8759</td>
-              <td>Brasília-DF</td>
-              <td><button type="button" class="btn btn-warning">Editar</button> <button type="button" class="btn btn-danger">Excluir</button></td>
-
-              </tr>
-              <td>Marcos</td>
-              <td>Silva</td>
-              <td>17890123456</td>
-              <td>Atendente</td>
-              <td>(61) 9 3645-8759</td>
-              <td>Brasília-DF</td>
-              <td><button type="button" class="btn btn-warning">Editar</button> <button type="button" class="btn btn-danger">Excluir</button></td>
-
-              </tr>
-              <td>Marcos</td>
-              <td>Silva</td>
-              <td>17890123456</td>
-              <td>Atendente</td>
-              <td>(61) 9 3645-8759</td>
-              <td>Brasília-DF</td>
-              <td><button type="button" class="btn btn-warning">Editar</button> <button type="button" class="btn btn-danger">Excluir</button></td>
-
             </tbody>
           </table>
         </div>
       </div>
-    </div>
-
-    <!--  EXEMPLO
-    <<Para adicionar mais Tabs, altere o id e o for de acordo com a qauntidade já existente>>
-      <div class="tab">
-      <input type="radio" id="tab-3" name="tab-group-1">
-      <label for="tab-3">Estoque</label>
-      <div class="content">
-        <h3>Estoque</h3>
-        <a style="text-decoration: none" href="#"><h5>Cadastrar Produto</h5></a>
-        <hr>
-      </div>
-    </div>
--->
-
-    <!-- Tab fornecedores-->
-    <div class="tab">
-      <input type="radio" id="tab-2" name="tab-group-1">
-      <label for="tab-2">Fornecedores</label>
-
-      <div class="content">
+      <!--Tabela fornecedores-->
+      <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+        <br>
         <h4>Fornecedores</h4>
         <a style="text-decoration: none" href="#">
           <h6>Cadastrar Fornecedor</h6>
         </a>
-        <hr>
         <div id="overflowTest">
           <table id="tablefunc" class="table">
             <thead>
@@ -181,18 +145,13 @@
           </table>
         </div>
       </div>
-    </div>
-
-    <!-- Tab estoque-->
-    <div class="tab">
-      <input type="radio" id="tab-3" name="tab-group-1">
-      <label for="tab-3">Estoque</label>
-      <div class="content">
+      <!--Tabela estoque-->
+      <div class="tab-pane fade" id="nav-estoque" role="tabpanel" aria-labelledby="nav-estoque-tab">
+        <br>
         <h4>Estoque</h4>
         <a style="text-decoration: none" href="#">
           <h6>Cadastrar Produto</h6>
         </a>
-        <hr>
         <div id="overflowTest">
           <table id="tablefunc" class="table">
             <thead>
@@ -232,8 +191,9 @@
       </div>
     </div>
   </div>
-  <!--footer-->
+  <!--Footer-->
   <footer>
+    <h4>Em Produção</h4>
   </footer>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
